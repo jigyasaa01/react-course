5a4aee287405d6b28c152f753d61687b17f9e931
jigyasaa01 <jigyasachourasia062@gmail.com>
2025-01-16 20:20:48 +0530

    Custom React

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,16 @@
 import Youtube from "./Youtube";
 
+
+const username = "hitesh"
 function App() {
   return (
     <>
-      <h1>Vite React App</h1>
+      <h1>Vite React App {2+2}</h1>
+      <h1>Vite React App {username}</h1>
       <Youtube />
     </>
   );
 }
+
 
 export default App;
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,7 +1,36 @@
-/* eslint-disable no-unused-vars */
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Youtube from './Youtube.jsx'
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to vist google'
+}
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target= "_blank">Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google'
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
