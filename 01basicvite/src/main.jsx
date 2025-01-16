e05514ea0b857a6f3ad0c42d7a4a0bb4fb80c140
jigyasaa01 <jigyasachourasia062@gmail.com>
2025-01-16 14:29:16 +0530

    Commit for vite and classic react

---
+++ 01basicvite/src/main.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
